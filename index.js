f7becc709223a679fb527414839cdc28512aa908
Gilles <gstcyr@gmail.com>
2022-02-01 11:21:48 -0700

    - update

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 import { NativeModules } from 'react-native';
 
+/**
+ * @property {function} BbotMdm.isManaged   - Returns null if not managed, otherwise the managing app id
+ */
 const { BbotMdm } = NativeModules;
 
 export default BbotMdm;
